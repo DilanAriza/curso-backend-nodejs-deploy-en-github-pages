891b7a0369d904af93eba0f55cd391ec60b35548
Dilan Ariza <dilansantiagoariza@gmail.com>
2020-10-19 17:16:45 -0500

    mini changes

--- webpack.config.js
+++ webpack.config.js
@@ -5,45 +5,49 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
         publicPath: '/',
     },
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: ['.js', '.jsx']
     },
     devServer: {
-        historyApiFallback: true
+        historyApiFallback: true,
     },
     module: {
         rules: [{
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
-            }
-        }, {
-            test: /\.html$/,
-            use: [{
-                loader: 'html-loader'
-            }]
-        }, {
-            test: /\.(s*)css$/,
-            use: [{
-                    loader: MiniCssExtractPlugin.loader,
-                },
-                'css-loader',
-                'sass-loader'
-            ]
-        }, {
-            test: /\.(png|gif|jpg)$/,
-            use: [{
-                loader: 'file-loader',
-                options: {
-                    name: 'assets/[hash].[ext]'
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
                 }
-            }]
-        }]
+            },
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: 'html-loader'
+                }]
+            },
+            {
+                test: /\.(s*)css$/,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|gif|jpg)$/,
+                use: [{
+                    'loader': 'file-loader',
+                    options: {
+                        name: 'assets/[hash].[ext]'
+                    }
+                }]
+            }
+        ]
     },
     plugins: [
         new HtmlWebPackPlugin({
@@ -52,7 +56,6 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: 'assets/[name].css'
-        })
+        }),
     ]
-
-}
+};
